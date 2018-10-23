09f1b5a716515227511160ea76512f8c13b0264e
Genevieve <genschultz@gmail.com>
2018-10-22 19:34:49 -0700

    added typography style packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Hello from './components/Hello'
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Hello from "./components/Hello";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <Hello name="Typescript" enthusiasmLevel={10}/>,
-  document.getElementById('root') as HTMLElement
+  <Hello name="Typescript" enthusiasmLevel={10} />,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
