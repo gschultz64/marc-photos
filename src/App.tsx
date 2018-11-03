14b457c6cd71355bf9b2c240f6184e4d620a99df
Genevieve <genschultz@gmail.com>
2018-11-02 17:50:46 -0700

    merged branches

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.min.css";
 import { GoogleFont, TypographyStyle } from "react-typography";
 import typography from "../src/utils/typography";
 import { BrowserRouter as Router } from "react-router-dom";
-import Sidebar from './components/Sidebar';
+import { Helmet } from "react-helmet";
+import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import { Topbar } from "./components/Topbar";
 
@@ -12,10 +13,18 @@ class App extends React.Component {
     return (
       <Router>
         <div className="App">
-      
+          <Helmet>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <title>Marc Schultz Photography</title>
+          </Helmet>
           <TypographyStyle typography={typography} />
           <GoogleFont typography={typography} />
-      
+
           <div className="entire-page">
             <Topbar />
             <div className="page-contents">
@@ -23,7 +32,6 @@ class App extends React.Component {
               <Main />
             </div>
           </div>
-
         </div>
       </Router>
     );
