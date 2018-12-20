53417ab6fe949553dcb3d8763ece73dcca44ee14
Genevieve Schultz <genschultz@gmail.com>
2018-12-20 12:38:58 -0800

    updated node packages

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -2,20 +2,20 @@ import * as React from "react";
 import axios from "axios";
 
 // Request API.
-// axios
-//   .get('http://localhost:1337/products', {
-//     params: {
-//       _sort: 'created_at:desc'
-//     }
-//   })
-//   .then(response => {
-//     // Handle success.
-//     console.log('Well done, here is the list of products: ', response.data);
-//   })
-//   .catch(error => {
-//     // Handle error.
-//     console.log('An error occurred:', error);
-//   });
+axios
+  .get('http://localhost:1337/products', {
+    params: {
+      _sort: 'created_at:desc'
+    }
+  })
+  .then(response => {
+    // Handle success.
+    console.log('Well done, here is the list of products: ', response.data);
+  })
+  .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error);
+  });
 
 class Test extends React.Component {
   constructor(props: any) {
